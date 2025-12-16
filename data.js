b50293369621a585b4fa46acc5159c9418caa323
NVX419 <opqrqa@gmail.com>
2025-12-16 17:43:06 +0300

    Add appData object with application details

---
+++ data.js
@@ -0,0 +1,11 @@
+const appData = {
+  name: "Arab Vision",
+  version: "1.0.0",
+  developer: "Arab Team",
+  features: [
+    "يشغل السكربتات",
+    "يدعم الحسابات الفيتنامية",
+    "سريع وخفيف",
+    "تصميم عربي"
+  ]
+};
