cb26cb6c0aa7d123fe8e1939e52770c510ce2590
NVX419 <opqrqa@gmail.com>
2025-12-19 20:10:35 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,10 @@
-// Ripple Effect
-document.querySelectorAll('.ripple-btn').forEach(button => {
-    button.addEventListener('click', function (e) {
+function buyWhatsapp() {
+    window.open(
+        "https://api.whatsapp.com/send/?phone=201011580685&text&type=phone_number&app_absent=0&wame_ctl=1",
+        "_blank"
+    );
+}
 
-        const circle = document.createElement('span');
-        const diameter = Math.max(this.clientWidth, this.clientHeight);
-        const radius = diameter / 2;
-
-        circle.style.width = circle.style.height = `${diameter}px`;
-        circle.style.left = `${e.clientX - this.getBoundingClientRect().left - radius}px`;
-        circle.style.top = `${e.clientY - this.getBoundingClientRect().top - radius}px`;
-
-        this.appendChild(circle);
-
-        setTimeout(() => circle.remove(), 600);
-
-        // Redirect
-        const link = this.getAttribute('data-link');
-        if (link) {
-            setTimeout(() => {
-                window.open(link, '_blank');
-            }, 300);
-        }
-    });
-});
-
-// Smooth Scroll
-document.querySelectorAll('nav a').forEach(anchor => {
-    anchor.addEventListener('click', function(e) {
-        e.preventDefault();
-        document.querySelector(this.getAttribute('href'))
-            .scrollIntoView({ behavior: 'smooth' });
-    });
-});
+function scrollToSection(id) {
+    document.getElementById(id).scrollIntoView({ behavior: "smooth" });
+}
